e95157f8880b51fb3c37f98634c00d92c2a6286e
adtandale <anandprakash44@gmail.com>
2021-08-30 14:40:53 +0530

    Update operator_ops.py

--- piqe_ocp_lib/api/tasks/operator_ops.py
+++ piqe_ocp_lib/api/tasks/operator_ops.py
@@ -1,7 +1,8 @@
 import logging
 from kubernetes.client.rest import ApiException
-from typing import Dict, List, Optional, Tuple, Union
 from kubernetes.client.rest import ApiException
+from typing import Dict, List, Optional, Tuple, Union
+
 
 from piqe_ocp_lib import __loggername__
 from piqe_ocp_lib.api.ocp_exceptions import UnsupportedInstallMode
