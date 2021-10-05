f95f471a0a6072e42ddfc4ef354f7f85a9a946a8
Anandprakash Tandale <atandale@redhat.com>
2021-10-05 19:47:51 +0530

    custom resource for local volume set

--- piqe_ocp_lib/api/crd/local_storage_operator.py
+++ piqe_ocp_lib/api/crd/local_storage_operator.py
@@ -144,3 +144,115 @@ class LocalVolume(LocalStorageOperator):
         else:
             logger.info("local volume is not create")
         return api_response
+
+
+class LocalVolumeSet(LocalStorageOperator):
+    """
+    LocalVolumeSet Class extends LocalStorageOperator
+    related to local volume set
+    :param kube_config_file: A kubernetes config file.
+    :return: None
+    """
+
+    def __init__(self, kube_config_file=None):
+        super().__init__(kube_config_file=kube_config_file)
+        self.api_version = "local.storage.openshift.io/v1alpha1"
+        self.kind = "LocalVolumeSet"
+        self.lvs = self.dyn_client.resources.get(api_version=self.api_version, kind=self.kind)
+
+    def create_local_volume_set(self, **kwargs) -> Optional[ResourceInstance]:
+        """
+        create local volume set
+        :param local_volume_set_name:(required) name  of the local volume
+
+        :return api response
+        """
+        if self.operator_version is not None and self.channel is not None and self.check_operator_install is True:
+            csv = ClusterServiceVersion()
+            csv_obj = csv.get_cluster_service_version(
+                "local-storage-operator." + self.operator_version, "openshift-local-storage"
+            )
+            crd = csv_obj.metadata.annotations["alm-examples"]
+            for i in range(0, len(eval(crd))):
+                if "'kind': 'LocalVolumeSet', 'metadata'" in str(eval(crd)[i]):
+                    target_item = i
+            body = eval(crd)[target_item]
+            if "name" in kwargs.keys():
+                body["metadata"]["name"] = kwargs["name"]
+            if "storageClassName" in kwargs.keys():
+                body["spec"]["storageClassName"] = kwargs["storageClassName"]
+            if "deviceTypes" in kwargs.keys():
+                body["spec"]["deviceInclusionSpec"]["deviceTypes"] = kwargs["deviceTypes"]
+            if "volumeMode" in kwargs.keys():
+                body["spec"]["volumeMode"] = kwargs["volumeMode"]
+            api_response = None
+            try:
+                # api_response = self.lvs.create(body=body)
+                api_response = self.lvs.create(namespace="openshift-local-storage", body=body)
+            except ApiException as e:
+                logger.exception(f"Exception while creating Local Volumeset : {e}\n")
+        else:
+            logger.info("local storage operator is not installed")
+        return api_response
+
+    def get_local_volume_set(self, namespace: str, local_volume_set_name: str) -> Optional[ResourceInstance]:
+        """
+        get local volume set
+        param namespace: namespace of the local volume
+        param local_volume_set_name: name of the local volume
+        return: api response
+        """
+        api_response = None
+        try:
+            api_response = self.lvs.get(namespace=namespace, name=local_volume_set_name)
+        except ApiException as e:
+            logger.exception(f"Exception while creating Local Volume : {e}\n")
+        return api_response
+
+    def watch_local_volume_set(self, namespace: str, local_volume_set_name: str, timeout: int) -> bool:
+        """
+        watch local volume set
+        param namespace: namespace of the local volume
+        param local_volume_set_name: name of the local volume
+        timeout: seconds for check
+        return: is local volume ready (True | FALSE)
+        """
+        end = time() + timeout
+        while time() < end:
+            api_response = self.get_local_volume_set(namespace=namespace, local_volume_set_name=local_volume_set_name)
+            if api_response.status is not None:
+                break
+            else:
+                logger.info(f"local volume set {local_volume_set_name} status is present, waiting for ready state")
+                continue
+        if len(api_response.status.conditions) != 0:
+            is_local_volume_set_ready = False
+            field_selector = f"metadata.name={local_volume_set_name}"
+            for event in self.lvs.watch(
+                namespace="openshift-local-storage", field_selector=field_selector, timeout=timeout
+            ):
+                for condition in event["object"]["status"]["conditions"]:
+                    if condition["status"] == "True":
+                        logger.info(f"local volume set {local_volume_set_name} status is ready")
+                        is_local_volume_set_ready = True
+                        return is_local_volume_set_ready
+                    else:
+                        logger.info(f"local volume{local_volume_set_name} is waiting for ready state")
+        else:
+            logger.error(f"status for {local_volume_set_name} volume is not present")
+        return is_local_volume_set_ready
+
+    def delete_local_volume_set(self, local_volume_set_name: str) -> Optional[ResourceInstance]:
+        """
+        delete local volume set
+        return: api response
+        """
+        if self.create_local_volume_set is not None:
+            api_response = None
+            try:
+                api_response = self.lvs.delete(name=local_volume_set_name, namespace="openshift-local-storage")
+            except ApiException as e:
+                logger.exception(f"Exception while deleting Local Volume set : {e}\n")
+        else:
+            logger.info("local volume is not create")
+        return api_response
--- piqe_ocp_lib/tests/crd/test_local_storage_operator.py
+++ piqe_ocp_lib/tests/crd/test_local_storage_operator.py
@@ -3,7 +3,7 @@ import logging
 import pytest
 
 from piqe_ocp_lib import __loggername__
-from piqe_ocp_lib.api.crd.local_storage_operator import LocalStorageOperator, LocalVolume
+from piqe_ocp_lib.api.crd.local_storage_operator import LocalStorageOperator, LocalVolume, LocalVolumeSet
 from piqe_ocp_lib.api.resources.ocp_operators import ClusterServiceVersion
 
 logger = logging.getLogger(__loggername__)
@@ -21,6 +21,7 @@ def get_test_objects(get_kubeconfig):
             self.csv_obj = ClusterServiceVersion(kube_config_file=get_kubeconfig)
             self.lso = LocalStorageOperator(kube_config_file=get_kubeconfig)
             self.lv = LocalVolume(kube_config_file=get_kubeconfig)
+            self.lvs = LocalVolumeSet(kube_config_file=get_kubeconfig)
 
     test_objs = TestObjects()
     return test_objs
@@ -33,9 +34,14 @@ def get_test_objects(get_kubeconfig):
     def local_volume(get_kubeconfig) -> LocalVolume:
         return LocalVolume(kube_config_file=get_kubeconfig)
 
+    @pytest.fixture(scope="module")
+    def local_volume_set(get_kubeconfig) -> LocalVolume:
+        return LocalVolumeSet(kube_config_file=get_kubeconfig)
+
 
 class TestLocalStorageOperator:
     lv_name = "testlvname"
+    lvs_name = "ttestlvsname"
 
     def test_create_local_volume(self, get_test_objects):
         api_response = get_test_objects.lv.create_local_volume(
@@ -59,3 +65,28 @@ class TestLocalStorageOperator:
 
     def test_delete_local_volume(self, get_test_objects):
         assert get_test_objects.lv.delete_local_volume(TestLocalStorageOperator.lv_name) is not None
+
+    def test_create_local_volume_set(self, get_test_objects):
+        api_response = get_test_objects.lvs.create_local_volume_set(
+            name=TestLocalStorageOperator.lvs_name,
+            storageClassName="teststoragelvsclass",
+            deviceTypes=["disk", "part"],
+            volumeMode="Block",
+        )
+
+    def test_get_local_volume_set(self, get_test_objects):
+        api_response = get_test_objects.lvs.get_local_volume_set(
+            "openshift-local-storage", TestLocalStorageOperator.lvs_name
+        )
+        assert api_response.kind == "LocalVolumeSet"
+
+    def test_watch_local_volume_set(self, get_test_objects):
+        assert (
+            get_test_objects.lvs.watch_local_volume_set(
+                "openshift-local-storage", TestLocalStorageOperator.lvs_name, timeout=120
+            )
+            is not False
+        )
+
+    def test_delete_local_volume_set(self, get_test_objects):
+        assert get_test_objects.lvs.delete_local_volume_set(TestLocalStorageOperator.lvs_name) is not None
